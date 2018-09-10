ecc8f6aa49a6c9428a13f795784b1c6deb9d4a82
Keiichii <artem.usov@gmail.com>
2018-09-10 11:37:19 +0300

    tuning 1-3

--- 1-3.py
+++ 1-3.py
@@ -5,12 +5,12 @@ for prev_letter, letter in enumerate(s[1:]):
     print('if', s[prev_letter], '<', letter)
     if s[prev_letter] <= letter:
         tmp += letter
-#        print('tmp:', tmp)
+        print('tmp:', tmp)
         if len(tmp) > len(ans):
             ans = tmp
     else:
         tmp = letter
-#    print('ans:', ans)
-    prev_letter += 1
+        print('tmp:', tmp)
+    print('ans:', ans)
 
 print('Longest substring in alphabetical order is:', ans)
